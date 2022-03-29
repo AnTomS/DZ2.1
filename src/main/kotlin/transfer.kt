520dfde11697d759396ede16ca35249ca44644be
Tomilov Algernon Andrey <thekeibee@yandex.ru>
2022-03-29 22:32:16 +0800

    commit

--- src/main/kotlin/transfer.kt
+++ src/main/kotlin/transfer.kt
@@ -1,10 +1,10 @@
+const val COMMISIONCOP: Int = 35 * 100
 fun main() {
     println("За переводы с любых карт комиссия 0.75%, минимум 35 рублей")
     print("Введите цифрами сумму в рублях")
     val amountRub: Int = readln().toInt()
     val amountCop: Int = amountRub * 100
-    val commission: Double = (amountCop * 0.75) / 100
-    val commissionCop: Int = 35 * 100
-    val result = if (commission < commissionCop) commissionCop else commission
+    val commission: Double = (amountCop * 0.75) % 100
+    val result = if (commission < COMMISIONCOP) COMMISIONCOP else commission
     print("Ваша комиссия составит : $result копеек")
 }
